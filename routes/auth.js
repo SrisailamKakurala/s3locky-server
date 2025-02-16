298e8702f73ea732ed1357659ad48e459069f0ed
srisailamkakurala <srisailamkakurala@gmail.com>
2025-02-16 14:58:15 +0530

    added salt

--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ router.post("/signup", async (req, res) => {
     if (existingUser) return res.status(400).json({ message: "User already exists" });
 
     // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, "lksjfldskjflkdsjlskaf");
 
     // Create new user
     const newUser = new User({ email, password: hashedPassword });
